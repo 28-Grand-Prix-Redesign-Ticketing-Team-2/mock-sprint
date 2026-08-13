e62671c4eecc615a06995e88b85ccd22c2bad3ff
wongedan0 <168263040+wongedan0@users.noreply.github.com>
2026-08-13 12:22:59 +1000

    fix: font Inter doe snot apply to the login page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Geist, Geist_Mono, Inter } from 'next/font/google'
 import { Providers } from '@/providers'
 import './globals.css'
 
@@ -13,6 +13,11 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 })
 
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin']
+})
+
 export const metadata: Metadata = {
   title: {
     template: `%s | ${process.env.NEXT_PUBLIC_APP_NAME ?? 'App'}`,
@@ -23,7 +28,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en" className={`${inter.variable} ${geistSans.variable} ${geistMono.variable}  h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
       </body>
